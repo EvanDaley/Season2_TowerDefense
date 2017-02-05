fa08d4200e0e4668daecd81ae4ca7223d656b074
Evan <daley.evan@gmail.com>
2017-02-05 10:36:53 -0800

    S1E4 Start

---
+++ TowerDefense/Assets/Scripts/BasicMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class BasicMovement : MonoBehaviour {
+
+	public NavMeshAgent navMeshAgent;
+	public Transform moveTarget;
+
+	void Start () 
+	{
+		navMeshAgent.destination = moveTarget.position;
+	}
+	
+	void Update () 
+	{
+		
+	}
+}
