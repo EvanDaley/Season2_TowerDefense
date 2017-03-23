40061f09c12ff1da0ca0219f63ea6718ac017f44
Evan <daley.evan@gmail.com>
2017-03-23 09:25:53 -0700

    S2E13 Start

--- TowerDefense/Assets/Scripts/BasicTargeting.cs
+++ TowerDefense/Assets/Scripts/BasicTargeting.cs
@@ -4,10 +4,25 @@ using UnityEngine;
 
 public class BasicTargeting : MonoBehaviour {
 
+	public float range = 8f;
+	public float rotationSpeed = 110f;
 	public Transform target;
-	
+	public bool shouldFire;
+
 	void Update () 
 	{
-		transform.LookAt (target);
+		float distance = Vector3.Distance (transform.position, target.position);
+
+		if (distance <= range)
+		{
+			Vector3 direction = transform.position - target.position;
+			Quaternion desiredRotation = Quaternion.LookRotation (direction, Vector3.up);
+			transform.rotation = Quaternion.RotateTowards (transform.rotation, desiredRotation, rotationSpeed * Time.deltaTime);
+			shouldFire = true;
+		} else
+		{
+			shouldFire = false;
+		}
+	
 	}
 }
---
+++ TowerDefense/Assets/Scripts/LaserTower.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserTower : MonoBehaviour {
+
+	public float damage;
+	public GameObject attackPrefab;
+	public BasicTargeting basicTargeting;
+
+	void Start () 
+	{
+		basicTargeting = GetComponent<BasicTargeting> ();
+	}
+	
+	void Update ()
+	{
+		if (basicTargeting.shouldFire)
+		{
+			print ("Should fire");
+		}
+	}
+}
