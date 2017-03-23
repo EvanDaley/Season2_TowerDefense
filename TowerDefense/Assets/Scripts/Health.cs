0b565cc0cdf3c90f028ac9ba4db44da23907cbc2
Evan <daley.evan@gmail.com>
2017-03-23 14:36:02 -0700

    S2E15 Start

---
+++ TowerDefense/Assets/Scripts/Health.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour {
+
+	public float health;
+
+	public void TakeDamage(float amount)
+	{
+		health -= amount;
+
+		if (health < 1)
+			Die ();
+	}
+
+	public void Die()
+	{
+		Destroy (gameObject);
+	}
+}
--- TowerDefense/Assets/Scripts/LaserTower.cs
+++ TowerDefense/Assets/Scripts/LaserTower.cs
@@ -8,6 +8,10 @@ public class LaserTower : MonoBehaviour {
 	public GameObject attackPrefab;
 	public GameObject attackInstance;
 	public BasicTargeting basicTargeting;
+	public LayerMask layerMask;
+
+	public float fireRate = .2f;
+	public float fireCooldown;
 
 	void Start () 
 	{
@@ -22,11 +26,35 @@ public class LaserTower : MonoBehaviour {
 		{
 			if (attackInstance.activeSelf == false)
 				attackInstance.SetActive (true);
+
+
+			// laser is active, rotating toward enemy
+			if (Time.time > fireCooldown)
+			{
+				CheckHit();
+				fireCooldown = Time.time + fireRate;
+			}
+
+
 		} else
 		{
 			if (attackInstance.activeSelf)
 				attackInstance.SetActive (false);
 		}
+	}
+
+	// fire a raycast, damage enemy
+	void CheckHit()
+	{
+		Ray ray = new Ray (transform.position, transform.forward);
+		RaycastHit hit; 
+
+		if (Physics.Raycast (ray, out hit, 100f, layerMask))
+		{
+			Health health = hit.transform.GetComponent<Health> ();
+			health.TakeDamage (damage);
+		}
+
 	}
 		
 }
